81b4d68a56a465df4779407f0583b67f080cd0dd
Amirrr <111552516+Amrbot01@users.noreply.github.com>
2022-09-01 20:51:59 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -24,7 +24,7 @@ const sentMsg  = await conn.sendMessage(
         }
     }
 )
-let jarot = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg'
+let jarot = 'https://i.postimg.cc/yNbj0db0/20220831-205142.jpg'
 await conn.send3ButtonImg(m.chat, jarot, `*Hai kak @${m.sender.split('@')[0]} 👋,  Itu nomor owner gw…*`, '📮: jngn di spam kack', '𝙎𝙚𝙬𝙖', '.sewa', '𝘽𝙖𝙘𝙠', '.menu', '𝘿𝙤𝙣𝙖𝙨𝙞', '.donasi', sentMsg)}
 handler.help = ['owner2', 'creator2']
 handler.tags = ['info']
